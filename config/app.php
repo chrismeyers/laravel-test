80435523b60436974c44261d6b4759dd490b109f
Chris Meyers <cm.02.93@gmail.com>
2020-07-02 17:38:33 -0600

    added debug_hide app config section.

--- config/app.php
+++ config/app.php
@@ -229,4 +229,19 @@ return [
 
     ],
 
+    'debug_hide' => [
+        '_ENV' => [
+            'APP_KEY',
+            'DB_PASSWORD',
+        ],
+
+        '_SERVER' => [
+            'APP_KEY',
+            'DB_PASSWORD',
+        ],
+
+        '_POST' => [
+            'password',
+        ],
+    ],
 ];
